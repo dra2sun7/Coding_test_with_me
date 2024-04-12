7a6e4365d4767cab0b9e01c96604db236819b81b
seomj <smj100394@gmail.com>
2024-04-12 14:59:29 +0900

    최소직사각형 / Level 1 / https://school.programmers.co.kr/learn/courses/30/lessons/86491

---
+++ Seo_Min_Jae/[PGS]/최소직사각형_level1.py
@@ -0,0 +1,9 @@
+def solution(sizes):
+    answer = 0
+    w = []
+    h = []
+    for size in sizes:
+        w.append(max(size))
+        h.append(min(size))
+    answer = max(w)*max(h)
+    return answer
