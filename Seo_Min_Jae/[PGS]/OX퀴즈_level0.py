f2eee448200d3b5fe66fca25edce44b786cead2f
dra2sun7 <dra2sun7@gmail.com>
2024-10-17 19:42:04 +0900

    브랜치 분할

--- Seo_Min_Jae/[PGS]/OX퀴즈_level0.py
+++
@@ -1,9 +0,0 @@
-def solution(quiz):
-    answer = []
-    for q in quiz:
-        cal, res = q.split("=")
-        if eval(cal) == int(res):
-            answer.append("O")
-        else:
-            answer.append("X")
-    return answer
--- Seo_Min_Jae/[PGS]/최고의 집합_level3.py
+++
@@ -1,9 +0,0 @@
-def solution(n, s):
-    answer = []
-    while n:
-        if s//n == 0:
-            return [-1]
-        answer.append(s//n)
-        s = s-(s//n)
-        n -= 1
-    return answer
