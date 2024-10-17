523494906189353c276c1f990c2e5100f6dccff4
dra2sun7 <dra2sun7@gmail.com>
2024-10-17 19:41:27 +0900

    브랜치 분할

--- Lee_Hyeon_Ho/Programmers/Sort/K번째수.py
+++
@@ -1,7 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for x in commands:
-        arr = array[x[0]-1:x[1]]
-        arr.sort()
-        answer.append(arr[x[2]-1])
-    return answer
--- Lee_Hyeon_Ho/SW_Expert_Academy/1_level/홀수만_더하기.py
+++
@@ -1,4 +0,0 @@
-x = int(input())
- 
-for i in range(x):
-    print(f"#{i+1} {sum(filter(lambda x: x%2 == 1, map(int, input().split())))}")
