d516d244ff1d0d2d44fdf564b79b23efb5288469
seomj <smj100394@gmail.com>
2025-05-22 15:41:37 +0900

    [추가] Ladder1 / D4 / https://swexpertacademy.com/main/main.do

--- Seo_Min_Jae/[ETC]/1210.py
+++ Seo_Min_Jae/[ETC]/1210.py
@@ -28,3 +28,41 @@ for test_case in range(1, T + 1):
             if ladder(i):
                 print(f"#{t} {i}")
                 break
+
+###########################################
+##### 아래에서부터 확인하는 방법으로 개선 ######
+###########################################
+
+def ladder(x, y):
+    visited = [[False]*100 for _ in range(100)]
+
+    while x > 0:
+        visited[x][y] = True
+
+        # 왼쪽
+        if y > 0 and graph[x][y-1] == 1 and not visited[x][y-1]:
+            while y > 0 and graph[x][y-1] == 1:
+                y -= 1
+                visited[x][y] = True
+        # 오른쪽
+        elif y < 99 and graph[x][y+1] and not visited[x][y+1]:
+            while y < 99 and graph[x][y+1] == 1:
+                y += 1
+                visited[x][y] = True
+
+        x -= 1
+
+    return y
+
+T = 10
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+
+    t = int(input())
+    graph = [list(map(int, input().split())) for _ in range(100)]
+
+    for i in range(100):
+        if graph[99][i] == 2:
+            res = ladder(99, i)
+    
+    print(f"#{t} {res}")
